97e9dac5bfb650d0af555c4fd233de8e3dd83805
Circa <circa@jkhub.org>
2022-07-25 10:04:23 -0400

    Update q_platform.h

--- shared/qcommon/q_platform.h
+++ shared/qcommon/q_platform.h
@@ -108,9 +108,6 @@ along with this program; if not, see <http://www.gnu.org/licenses/>.
 		#define idx64
 		#define ARCH_STRING "x86_64"
 		#define Q3_LITTLE_ENDIAN
-	#elif defined(__arm64__)
-  		#define ARCH_STRING "arm64"
-        #define Q3_LITTLE_ENDIAN
     #endif
 
     #define DLL_EXT ".dylib"
